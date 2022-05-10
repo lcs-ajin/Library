771682e8356f3a757cccc31c4a2dd5e754451d85
Anthony Jin <ajin@lcs.0n.ca>
2022-05-10 09:46:49 -0400

    Initial Commit

---
+++ Library/LibraryApp.swift
@@ -0,0 +1,17 @@
+//
+//  LibraryApp.swift
+//  Library
+//
+//  Created by Anthony Jin on 2022-05-10.
+//
+
+import SwiftUI
+
+@main
+struct LibraryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
